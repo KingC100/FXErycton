ad349dbc3b1a00451b2a3516fc6eebd2d847b77b
KingC100 <kingcrimson96@gmail.com>
2013-12-15 10:59:27 +0900

    画面起動確認

--- src/fxerycton/FXErycton.java
+++ src/fxerycton/FXErycton.java
@@ -7,17 +7,9 @@
 package fxerycton;
 
 import javafx.application.Application;
-<<<<<<< HEAD
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-=======
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
->>>>>>> origin/master
 import javafx.stage.Stage;
 
 /**
@@ -26,8 +18,6 @@ import javafx.stage.Stage;
  */
 public class FXErycton extends Application {
     
-    @Override
-<<<<<<< HEAD
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXErycton.fxml"));
         
@@ -35,27 +25,6 @@ public class FXErycton extends Application {
         
         stage.setScene(scene);
         stage.show();
-=======
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
->>>>>>> origin/master
     }
 
     /**
